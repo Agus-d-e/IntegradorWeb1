e64a489967acdb4d63f9b8b89368bbcac11e9a91
josemir01 <josemirzaleh45@gmail.com>
2025-10-27 19:42:06 -0300

    validaciones corregidas

--- Scripts/FormularioContacto.js
+++ Scripts/FormularioContacto.js
@@ -25,15 +25,27 @@ function ValidarFormulario(event){
  }
 
  //validar email
- let patronEmail = /(\w+)+@(gmail|hotmail|yahoo|test)(\.com|\.edu|\.ar){1,2}/g;
- if(!patronEmail.test(email)){
-    document.getElementById("error-correo").textContent="el correo debe tener una estructura algo@gmail.com"
+ let patronEmail =/(\w+)+@(gmail|hotmail|yahoo|test)(\.com|\.edu|\.ar){1,2}/;
+ if(correo===""){
+    document.getElementById("error-correo").textContent = "El correo es obligatorio";
+    document.getElementById("error-correo").style.color = "red";
+ }else if(!patronEmail.test(correo)){
+    document.getElementById("error-correo").textContent="el correo debe tener una estructura 'algo@gmail.com'"
+    document.getElementById("error-correo").style.color="red"
+ }else{
+   document.getElementById("error-correo").textContent=""
  }
 
+ 
  //validar telefono
 let patron = /^[0-9]{10,15}$/;
- if(!patron.test(telefono)){
-   document.getElementById("error-telefono").textContent="ingrese un numero de telefono valido(digitos de 10 a 15 caracteres)"
- }
-
+ if (telefono === "") {
+    document.getElementById("error-telefono").textContent = "El teléfono es obligatorio";
+    document.getElementById("error-telefono").style.color = "red";
+} else if(!patronTel.test(telefono)) {
+    document.getElementById("error-telefono").textContent = "Ingrese un teléfono válido (10 a 15 dígitos)";
+    document.getElementById("error-telefono").style.color = "red";
+} else {
+    document.getElementById("error-telefono").textContent = "";
+}
 }
